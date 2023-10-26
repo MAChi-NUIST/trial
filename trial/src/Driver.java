141894879e847a4a3dac9aefae507542b582fb07
Kobe-Bryant-fuhuo <jbyx555@163.com>
2023-10-26 20:10:03 +0800

    234

--- trial/src/Driver.java
+++ trial/src/Driver.java
@@ -2,5 +2,6 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("fuck you");
+        System.out.println("fuck you");
     }
 }
